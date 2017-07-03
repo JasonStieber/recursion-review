c1159667c6811c7e8da084f15fd2c779d05bb8c0
Hack Reactor Student <student@hackreactor.com>
2017-07-03 16:21:53 -0700

    Updated all spec files to allow tests to pass.

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -6,9 +6,11 @@ describe('parseJSON', function() {
       var result = parseJSON(test);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
+      if (!equality) { console.log(test); }
+      expect(equality).to.equal(true);
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      // throw new Error('Test is missing.')
     });
   });
 
