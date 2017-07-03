f92de5a77d2e4317f777851d0da7f552648510b3
Hack Reactor Student <student@hackreactor.com>
2017-07-03 13:29:22 -0700

    Implement get Elements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -12,11 +12,7 @@ var isClass = function(className, node) {
 };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, currentDir) { 
-// your code here
-  if (currentDir === undefined) {
-    return getElementsByClassName(className, document.body);
-  }
+var getElementsByClassName = function(className, currentDir = document.body) { 
   var matchedNodes = isClass(className, currentDir);
   var numberOfChildren = currentDir.childNodes.length === 0 ? 0 : 
     currentDir.childNodes.length;
@@ -26,5 +22,4 @@ var getElementsByClassName = function(className, currentDir) {
   }
   matchedNodes.concat(isClass(className, currentDir));
   return matchedNodes;
-   
 };
