dadf03c6f3d6abf57591dd02a0271d936f8cfe1d
Hack Reactor Student <student@hackreactor.com>
2017-07-03 13:25:18 -0700

    Get Element by class name is up and running. StringifyJSON is not yet running.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,28 @@
 //   return document.getElementsByClassName(className);
 // };
 
+var isClass = function(className, node) {
+  if (node.classList !== undefined && node.classList.contains(className)) {
+    return [node];
+  } else {
+    return [];
+  }
+};
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, currentDir) { 
+// your code here
+  if (currentDir === undefined) {
+    return getElementsByClassName(className, document.body);
+  }
+  var matchedNodes = isClass(className, currentDir);
+  var numberOfChildren = currentDir.childNodes.length === 0 ? 0 : 
+    currentDir.childNodes.length;
+  for (var i = 0; i < numberOfChildren; i++) {
+    matchedNodes = matchedNodes.concat(getElementsByClassName(className, 
+      currentDir.childNodes[i]));
+  }
+  matchedNodes.concat(isClass(className, currentDir));
+  return matchedNodes;
+   
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  switch (typeof obj) {
+    case 'boolean':  
+    case 'number':
+      return obj.toString();
+      break;
+    case 'string':
+      return '"' + obj.toString() + '"';
+      break;
+    case 'undefined':
+      return '{}';
+      break;
+    case 'function':
+      return 'undefined';
+      break;
+    case 'object':
+      if (obj === null) {
+        return 'null';
+      } else if (Array.isArray(obj)) {
+        let objectString = '[';
+        for (const element of obj) {
+          if (objectString !== '[') {
+            objectString += ',';
+          }
+          objectString += stringifyJSON(element);
+        }
+        return objectString + ']';
+      } else {
+        let objectString = '{';
+        for (let i = 0; i < Object.keys(obj).length; i++) {
+          if (Object.keys(obj)[i] === undefined) { return '{}'; }
+          if (objectString !== '{') {
+            objectString += ',';
+          }
+          objectString += property + ':';
+          objectString += obj[property]; 
+        }  
+        objectString += '}';
+      }
+      break;
+    default:
+      return undefined;  
+      break;
+  }
 };
